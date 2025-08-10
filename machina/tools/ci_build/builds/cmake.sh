dd9d317c26aeb1bedd61da80b25570d61ea1395b
tuncayakberli <tuncayakberli@gmail.com>
2025-08-11 00:10:51 +0400

    patches

--- machina/tools/ci_build/builds/cmake.sh
+++ machina/tools/ci_build/builds/cmake.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+###############################################################################
+#                                                                             #
+#   Copyright (c) 2025, NeXTHub Corporation. All Rights Reserved.             #
+#   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.             #
+#                                                                             #
+#   Author: Tunjay Akbarli                                                    #
+#   Date: Saturday, May 31, 2025.                                             #
+#                                                                             #
+#   Licensed under the Apache License, Version 2.0 (the "License");           #
+#   you may not use this file except in compliance with the License.          #
+#   You may obtain a copy of the License at:                                  #
+#                                                                             #
+#       http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                             #
+#   Unless required by applicable law or agreed to in writing, software       #
+#   distributed under the License is distributed on an "AS IS" BASIS,         #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#   See the License for the specific language governing permissions and       #
+#   limitations under the License.                                            #
+#                                                                             #
+#   Please contact NeXTHub Corporation, 651 N Broad St, Suite 201,            #
+#   Middletown, DE 19709, New Castle County, USA.                             #
+#                                                                             #
+###############################################################################
 
 set -e
 
